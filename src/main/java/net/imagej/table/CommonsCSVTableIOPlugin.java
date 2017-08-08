1032a957f5f9c657050afa424bfb71dd967e5a75
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2017-08-08 15:03:22 +0200

    Fix license headers

--- src/main/java/net/imagej/table/CommonsCSVTableIOPlugin.java
+++ src/main/java/net/imagej/table/CommonsCSVTableIOPlugin.java
@@ -1,10 +1,9 @@
 /*
  * #%L
- * SciJava Common shared library for SciJava software.
+ * ImageJ software for multidimensional image processing and analysis.
  * %%
- * Copyright (C) 2009 - 2016 Board of Regents of the University of
- * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
- * Institute of Molecular Cell Biology and Genetics.
+ * Copyright (C) 2017 Board of Regents of the University of
+ * Wisconsin-Madison and University of Konstanz.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
