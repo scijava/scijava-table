7a0f828c0fa052a1bcfb4aa75c562f042be251b1
Curtis Rueden <ctrueden@wisc.edu>
2018-12-21 15:09:32 -0600

    Add method to wrap list of maps as a table
    This will be useful in script languages like Groovy,
    so that (small) tables can be written succinctly:
    
      table = Tables.wrap([
        ["Town": "Shanghai", "Population": 24_256_800],
        ["Town": "Karachi", "Population": 23_500_000],
        ["Town": "Beijing", "Population": 21_516_000],
        ["Town": "Sao Paolo", "Population": 21_292_893],
      ])

--- src/main/java/org/scijava/table/Tables.java
+++ src/main/java/org/scijava/table/Tables.java
@@ -30,6 +30,11 @@
 
 package org.scijava.table;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * Utility methods for constructing tables.
  * 
@@ -39,6 +44,99 @@ public final class Tables {
 
 	private Tables() {
 		// NB: Prevent instantiation of utility class.
+	}
+
+	/**
+	 * Creates a table wrapping a list of maps. Each map is one row of the table.
+	 * Map keys are column names; map values are cell data.
+	 * <p>
+	 * Note that columns are inferred from the first map/row of the table, not
+	 * unioned across all rows.
+	 * </p>
+	 * 
+	 * @param data The data to wrap. Each list element is a row; maps go from
+	 *          column name to data.
+	 * @param rowHeaders List of row header labels. Pass null for no row headers.
+	 * @return A {@link Table} object wrapping the data structure.
+	 */
+	public static <T> Table<Column<T>, T> wrap(
+		final List<? extends Map<?, ? extends T>> data, final List<String> rowHeaders)
+	{
+		if (data.isEmpty()) throw new IllegalArgumentException("Cannot wrap an empty list");
+
+		return new ReadOnlyTable<T>() {
+
+			@Override
+			public int getRowCount() {
+				return data.size();
+			}
+
+			@Override
+			public String getRowHeader(final int row) {
+				if (rowHeaders == null || rowHeaders.size() < row) return null;
+				return rowHeaders.get(row);
+			}
+
+			@Override
+			public int size() {
+				return data.get(0).size();
+			}
+
+			@Override
+			public Column<T> get(final int col) {
+				class ColumnAccessor implements ReadOnlyColumn<T> {
+
+					private final Object tableData = data;
+					private boolean initialized;
+					private String colHeader;
+
+					@Override
+					public String getHeader() {
+						if (!initialized) {
+							int c = 0;
+							for (final Object key : data.get(0).keySet()) {
+								if (col == c++) {
+									colHeader = key == null ? "<null>" : key.toString();
+									break;
+								}
+							}
+							initialized = true;
+						}
+						return colHeader;
+					}
+
+					@Override
+					public int size() {
+						return data.size();
+					}
+
+					@Override
+					public Class<T> getType() {
+						// TODO: Consider whether this is terrible.
+						throw new UnsupportedOperationException();
+					}
+
+					@Override
+					public T get(final int index) {
+						return data.get(index).get(getHeader());
+					}
+
+					@Override
+					public int hashCode() {
+						return getHeader().hashCode();
+					}
+
+					@Override
+					public boolean equals(final Object obj) {
+						if (!(obj instanceof ColumnAccessor)) return false;
+						final ColumnAccessor other = ((ColumnAccessor) obj);
+						if (data != other.tableData) return false;
+						return Objects.equals(getHeader(), other.getHeader());
+					}
+				}
+				return new ColumnAccessor();
+			}
+		};
 	}
 
 	// -- Internal methods --
@@ -97,5 +195,159 @@ public final class Tables {
 		}
 		throw new IndexOutOfBoundsException("Invalid " + name + "s: " + index +
 			" - " + last);
+	}
+
+	private static UnsupportedOperationException readOnlyException() {
+		return new UnsupportedOperationException("Wrapped table is read-only");
+	}
+
+	// -- Helper classes --
+
+	/** Read-only version of a {@link Table}. */
+	private static interface ReadOnlyTable<T> extends Table<Column<T>, T> {
+
+		@Override
+		default void setColumnCount(final int colCount) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default List<Column<T>> insertColumns(final int col, final int count) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void setRowCount(final int rowCount) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void removeRows(int row, int count) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void setRowHeader(int row, String header) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void set(String colHeader, int row, T value) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean add(final Column<T> column) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean remove(final Object column) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean addAll(Collection<? extends Column<T>> c) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean addAll(int col, Collection<? extends Column<T>> c) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean removeAll(Collection<?> c) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean retainAll(Collection<?> c) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void clear() {
+			throw readOnlyException();
+		}
+
+		@Override
+		default Column<T> set(int col, Column<T> column) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void add(int col, Column<T> column) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default Column<T> remove(int col) {
+			throw readOnlyException();
+		}
+	}
+
+	/** Read-only version of a {@link Column}. */
+	private static interface ReadOnlyColumn<T> extends Column<T> {
+
+		@Override
+		default boolean add(final T e) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean remove(Object o) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean addAll(Collection<? extends T> c) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean addAll(int index, Collection<? extends T> c) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean removeAll(Collection<?> c) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default boolean retainAll(Collection<?> c) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void clear() {
+			throw readOnlyException();
+		}
+
+		@Override
+		default T set(int index, T element) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void add(int index, T element) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default T remove(int index) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void setHeader(String header) {
+			throw readOnlyException();
+		}
+
+		@Override
+		default void setSize(int size) {
+			throw readOnlyException();
+		}
 	}
 }
---
+++ src/test/java/org/scijava/table/TablesTest.java
@@ -0,0 +1,107 @@
+/*
+ * #%L
+ * Table structures for SciJava.
+ * %%
+ * Copyright (C) 2012 - 2018 Board of Regents of the University of
+ * Wisconsin-Madison, and Friedrich Miescher Institute for Biomedical Research.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.table;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link Tables}.
+ *
+ * @author Curtis Rueden
+ */
+public class TablesTest {
+
+	@Test
+	public void testWrap() {
+		final List<Map<Object, Object>> towns = Arrays.asList( //
+			map("Town", "Shanghai", "Population", 24_256_800), //
+			map("Town", "Karachi", "Population", 23_500_000), //
+			map("Town", "Beijing", "Population", 21_516_000), //
+			map("Town", "Sao Paolo", "Population", 21_292_893));
+
+		final List<String> rowHeaders = Arrays.asList("A", "B", "C", "D");
+
+		final Table<Column<Object>, Object> table = Tables.wrap(towns, rowHeaders);
+
+		// check table dimensions
+		assertEquals(2, table.size());
+		assertEquals(2, table.getColumnCount());
+		assertEquals(4, table.getRowCount());
+
+		// check row headers
+		for (int r = 0; r < rowHeaders.size(); r++)
+			assertEquals(rowHeaders.get(r), table.getRowHeader(r));
+
+		// check direct data access
+		assertEquals("Shanghai", table.get(0, 0));
+		assertEquals("Karachi", table.get(0, 1));
+		assertEquals("Beijing", table.get(0, 2));
+		assertEquals("Sao Paolo", table.get(0, 3));
+		assertEquals(24_256_800, table.get(1, 0));
+		assertEquals(23_500_000, table.get(1, 1));
+		assertEquals(21_516_000, table.get(1, 2));
+		assertEquals(21_292_893, table.get(1, 3));
+
+		// check first column
+		assertEquals("Town", table.getColumnHeader(0));
+		final Column<Object> townColumn = table.get("Town");
+		assertEquals(4, townColumn.size());
+		assertEquals("Shanghai", townColumn.get(0));
+		assertEquals("Karachi", townColumn.get(1));
+		assertEquals("Beijing", townColumn.get(2));
+		assertEquals("Sao Paolo", townColumn.get(3));
+		assertEquals(townColumn, table.get(0));
+
+		// check second column
+		assertEquals("Population", table.getColumnHeader(1));
+		final Column<Object> popColumn = table.get("Population");
+		assertEquals(4, popColumn.size());
+		assertEquals(24_256_800, popColumn.get(0));
+		assertEquals(23_500_000, popColumn.get(1));
+		assertEquals(21_516_000, popColumn.get(2));
+		assertEquals(21_292_893, popColumn.get(3));
+		assertEquals(popColumn, table.get(1));
+	}
+
+	private Map<Object, Object> map(final Object... kv) {
+		final LinkedHashMap<Object, Object> map = new LinkedHashMap<>();
+		for (int i = 0; i < kv.length; i += 2)
+			map.put(kv[i], kv[i + 1]);
+		return map;
+	}
+}
